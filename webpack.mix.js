033ea322e396a170f1f15b7991e8fc2572dd00f2
Matilda Smets <matilda.smets@student.gildeopleidingen.nl>
2024-10-16 14:47:58 +0200

    Fixes en toevoeging tailwind css en preline

---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+const mix = require('laravel-mix');
+
+mix.js("resources/js/app.js", "public/js")
+  .postCss("resources/css/app.css", "public/css", [
+    require("tailwindcss"),
+]);
+
+
+mix.copy('node_modules/preline/dist/preline.js', 'public/js/preline.js');

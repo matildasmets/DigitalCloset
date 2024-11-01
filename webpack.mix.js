e46e4638fb032b042a7e4fb79ecadd951239cf4b
Matilda Smets <matilda.smets@student.gildeopleidingen.nl>
2024-11-01 10:44:05 +0100

    Assets map toegevoegd

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,11 @@
 const mix = require('laravel-mix');
 
-mix.js("resources/js/app.js", "public/js")
-  .postCss("resources/css/app.css", "public/css", [
-    require("tailwindcss"),
-  ])
-  .copyDirectory("public/css/fonts", "public/fonts") 
-  .copy('node_modules/preline/dist/preline.js', 'public/js/preline.js')
-  .version()
-  .sourceMaps();
+mix.js("resources/js/**/*.js", "public/assets/js")
+.sass("resources/css/main.scss", "public/assets/css")
+.options({
+    postCss: [require("tailwindcss")],
+})
+.copyDirectory("resources/fonts", "public/assets/fonts")
+.copy('node_modules/preline/dist/preline.js', 'public/js/preline.js');
+
 

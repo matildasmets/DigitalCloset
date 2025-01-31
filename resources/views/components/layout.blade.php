48a7045ee06c97cb40a3391e0cdabe14f145df7a
issatillie <uarpnn@gmail.com>
2025-01-31 09:06:14 +0100

    Fix auth

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -24,7 +24,7 @@
           </div>
 
 
-        @if(session('user_id'))
+        @auth
             <div id="hs-base-header" class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block" aria-labelledby="hs-base-header-collapse">
                 <div class="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300">
                     <div class="py-2 md:py-0 flex flex-col md:flex-row md:items-center md:justify-end gap-0.5 md:gap-1">
@@ -35,7 +35,7 @@
                     </div>
                 </div>
             </div>
-        @endif
+        @endauth
 
         </nav>
     </header>
